d5cf339b08e84658a15855cd573537e52566d27d
kunalpatel <kunalpatel446@gmail.com>
2016-04-03 03:08:46 -0400

    DRUMF

---
+++ HackBCAProject/app/src/main/java/com/example/kunalpatel/hackbcaproject/accReader.java
@@ -0,0 +1,33 @@
+package com.example.kunalpatel.hackbcaproject;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+/**
+ * Created by kunalpatel on 4/3/16.
+ */
+public abstract class accReader extends Activity implements SensorEventListener {
+    private final SensorManager sensorManager;
+    private final Sensor acc;
+    public accReader()
+    {
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    }
+    public void onSensorChanged(SensorEvent event){
+    }
+
+}
